90158de906da039f24f48d0fc3782399e5c24099
Davide Raro <info@davideraroconsulting.com>
2020-03-23 15:21:09 +0200

    added firebase auth

--- src/App.js
+++ src/App.js
@@ -3,45 +3,69 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 import Header from './components/header/header.component'
-import {Switch, Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      currentUser: null
+        this.state = {
+            currentUser: null
+        }
     }
-  }
-  unsubscribefromAuth = null
+    unsubscribefromAuth = null
 
-  componentDidMount() {
-    this.unsubscribefromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user});
-    })
-  }
+    componentDidMount() {
+        this.unsubscribefromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth)
 
-  comnentWillUnmount() {
-    this.unsubscribefromAuth()
-  }
-  render() {
-    return (
-      <div className='App'>
-        <Header currentUser={this.state.currentUser}/>
-        
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage}/>
-          <Route path='/signin' component={SignInAndSignUp}/>
-        </Switch>
-      </div>
-    );
-  }
+                userRef.onSnapshot(snapShot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data()
+                        }
+                    }, () => { console.log(this.state) })
+                })
+
+            } else {
+                this.setState({ currentUser: userAuth })
+            }
+        })
+    }
+
+    comnentWillUnmount() {
+        this.unsubscribefromAuth()
+    }
+    render() {
+        return ( <
+            div className = 'App' >
+            <
+            Header currentUser = { this.state.currentUser }
+            />
+
+            <
+            Switch >
+            <
+            Route exact path = '/'
+            component = { HomePage }
+            /> <
+            Route path = '/shop'
+            component = { ShopPage }
+            /> <
+            Route path = '/signin'
+            component = { SignInAndSignUp }
+            /> < /
+            Switch > <
+            /div>
+        );
+    }
 }
 
 export default App;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,27 @@ const config = {
     messagingSenderId: "1043539153431",
     appId: "1:1043539153431:web:f10007b61552e2f1878252"
 };
+export const createUserProfileDocument = async(userAuth, additionalData) => {
+    if (!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    const snapShot = await userRef.get()
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+    return userRef
+};
 
 firebase.initializeApp(config)
 
