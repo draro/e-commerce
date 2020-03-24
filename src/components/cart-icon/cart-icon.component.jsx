bec05fc19c645742962ca2217a3e915ad36ef1bf
Davide Raro <info@davideraroconsulting.com>
2020-03-24 15:23:15 +0200

    update with reselect

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,14 +5,21 @@ import {ReactComponent as ShoppingIcon} from '../../assets/cart.svg'
 // redux
 import {connect} from 'react-redux'
 import {toggleCartHidden} from '../../redux/cart/cart.action'
+import { selectCartItemsCount} from '../../redux/cart/cart.selector'
 
-const CartIcon = ({toggleCartHidden}) => (
+
+
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 )
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
-export default connect(null, mapDispatchToProps)(CartIcon)
+
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
+})
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
